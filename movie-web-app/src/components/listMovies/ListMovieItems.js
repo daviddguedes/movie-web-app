6b08f28bb9d6f900e5d3ad87ab4c829d02461f84
Coelho Guedes <david.coelho.guedes@accenture.com>
2019-04-16 10:07:14 -0300

    CSS da lista de filmes

--- movie-web-app/src/components/listMovies/ListMovieItems.js
+++ movie-web-app/src/components/listMovies/ListMovieItems.js
@@ -53,17 +53,17 @@ class ListMovieItems extends Component {
                <ListGroup>
                   {this.props.movies.results.slice(0, 7).map(item => (
                      <ListGroupItem key={item.id} onClick={() => this.toggle(item)}>
-                        <Media>
+                        <Media className="row-relative">
                            <Media left href="#">
                               <img className="img-thumbnail img-responsive" style={{ width: 100 }} src={item.poster_path ? IMAGE_URL + `${item.poster_path}` : IMAGE_URL + `${item.backdrop_path}`} />
                            </Media>
-                           <Media body className="pl-3 row-relative">
+                           <Media body className="pl-3">
                               <h5>
                                  {item.title}
                               </h5>
                               {/* <p>{item.overview.substr(0, 150).concat('...')}</p> */}
-                              <p>Release date: {item.release_date}</p>
-                              <p className="text-muted ts-4">Genre: {this.getGenre(item.genre_ids)}</p>
+                              <p className="text-warning ts-4">Release date: {item.release_date}</p>
+                              <p className="text-muted ts-4 posB-0">Genre: {this.getGenre(item.genre_ids)}</p>
                            </Media>
                         </Media>
                      </ListGroupItem>
@@ -74,17 +74,17 @@ class ListMovieItems extends Component {
                <ListGroup>
                   {this.props.movies.results.slice(7, 14).map(item => (
                      <ListGroupItem key={item.id} onClick={() => this.toggle(item)}>
-                        <Media>
+                        <Media className="row-relative">
                            <Media left href="#">
                               <img className="img-thumbnail img-responsive" style={{ width: 100 }} src={item.poster_path ? IMAGE_URL + `${item.poster_path}` : IMAGE_URL + `${item.backdrop_path}`} />
                            </Media>
-                           <Media body className="pl-3 row-relative">
+                           <Media body className="pl-3">
                               <h5>
                                  {item.title}
                               </h5>
                               {/* <p>{item.overview.substr(0, 150).concat('...')}</p> */}
-                              <p>Release date: {item.release_date}</p>
-                              <p className="text-muted ts-4">Genre: {this.getGenre(item.genre_ids)}</p>
+                              <p className="text-warning ts-4">Release date: {item.release_date}</p>
+                              <p className="text-muted ts-4 posB-0">Genre: {this.getGenre(item.genre_ids)}</p>
                            </Media>
                         </Media>
                      </ListGroupItem>
@@ -95,7 +95,7 @@ class ListMovieItems extends Component {
                <ListGroup>
                   {this.props.movies.results.slice(14).map(item => (
                      <ListGroupItem key={item.id} onClick={() => this.toggle(item)}>
-                        <Media>
+                        <Media className="row-relative">
                            <Media left href="#">
                               <img className="img-thumbnail img-responsive" style={{ width: 100 }} src={item.poster_path ? IMAGE_URL + `${item.poster_path}` : IMAGE_URL + `${item.backdrop_path}`} />
                            </Media>
@@ -104,8 +104,8 @@ class ListMovieItems extends Component {
                                  {item.title}
                               </h5>
                               {/* <p>{item.overview.substr(0, 150).concat('...')}</p> */}
-                              <p>Release date: {item.release_date}</p>
-                              <p className="text-muted ts-4">Genre: {this.getGenre(item.genre_ids)}</p>
+                              <p className="text-warning ts-4">Release date: {item.release_date}</p>
+                              <p className="text-muted ts-4 posB-0">Genre: {this.getGenre(item.genre_ids)}</p>
                            </Media>
                         </Media>
                      </ListGroupItem>
